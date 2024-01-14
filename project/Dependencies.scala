6d707a60ae9d99170a858785d7896b9fb70f56c4
Scala Steward <scala-steward@users.noreply.github.com>
2024-01-14 05:16:57 +0000

    Update jsonpath to 0.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
     val circeJackson = "0.14.0"
     val scalaForex = "1.0.0"
     val scalaWeather = "1.0.0"
-    val gatlingJsonpath = "0.6.14"
+    val gatlingJsonpath = "0.7.0"
     val scalaUri = "1.5.1"
     val badRows = "2.1.2"
     val igluClient = "1.5.0"
