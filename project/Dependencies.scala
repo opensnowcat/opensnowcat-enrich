ff0ab243084b0a91ff035f4cb2ed33a351a04b16
Scala Steward <scala-steward@users.noreply.github.com>
2024-01-14 03:50:59 +0000

    Update scala-forex to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -58,7 +58,7 @@ object Dependencies {
     val circeOptics = "0.14.1"
     val circeConfig = "0.7.0"
     val circeJackson = "0.14.0"
-    val scalaForex = "1.0.0"
+    val scalaForex = "3.0.0"
     val scalaWeather = "1.0.0"
     val gatlingJsonpath = "0.6.14"
     val scalaUri = "1.5.1"
