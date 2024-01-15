b6cb74ee3aa88f7a1ecddc2cf633ba40f11818e1
Scala Steward <scala-steward@users.noreply.github.com>
2024-01-15 04:01:39 +0000

    Update log4j-to-slf4j to 2.22.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
     val jsonsmart = "2.4.10"
     val iabClient = "0.2.0"
     val yauaa = "7.11.0"
-    val log4jToSlf4j = "2.18.0"
+    val log4jToSlf4j = "2.22.1"
     val guava = "28.2-jre"
     val slf4j = "2.0.3"
     val log4j = "2.20.0"
