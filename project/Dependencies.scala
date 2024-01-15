a38c2b1ab852b6fa81c1ff70dfe76cd6d52cb235
Scala Steward <scala-steward@users.noreply.github.com>
2024-01-15 02:00:38 +0000

    Update scala-maxmind-iplookups to 0.8.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,7 +53,7 @@ object Dependencies {
     val protobuf = "3.21.12"
 
     val refererParser = "1.1.0"
-    val maxmindIplookups = "0.7.2"
+    val maxmindIplookups = "0.8.1"
     val circe = "0.14.1"
     val circeOptics = "0.14.1"
     val circeConfig = "0.7.0"
