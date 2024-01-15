10dc628e1ac0d3ddd786f6d754b2053bd37aa47c
Joao Correia <joao.correia@gmail.com>
2024-01-14 23:01:11 -0800

    Update libthrift to 0.19.0 (#73)
    Co-authored-by: Scala Steward <scala-steward@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
     val guava = "33.0.0-jre"
     val slf4j = "2.0.3"
     val log4j = "2.22.1"
-    val thrift = "0.15.0"
+    val thrift = "0.19.0"
     val sprayJson = "1.3.6"
     val netty = "4.1.104.Final"
     val protobuf = "3.25.2"
