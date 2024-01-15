31a98ff342bdccc59387c9ac21aa02b8488ba871
Scala Steward <scala-steward@users.noreply.github.com>
2024-01-15 02:59:47 +0000

    Update yauaa to 7.24.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies {
     val jaywayJsonpath = "2.7.0"
     val jsonsmart = "2.4.10"
     val iabClient = "0.2.0"
-    val yauaa = "7.11.0"
+    val yauaa = "7.24.0"
     val log4jToSlf4j = "2.18.0"
     val guava = "28.2-jre"
     val slf4j = "2.0.3"
