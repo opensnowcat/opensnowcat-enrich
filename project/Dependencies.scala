62d6d33e3a6d0fa5da630a6edd1a968b6cf8c4c4
Scala Steward <scala-steward@users.noreply.github.com>
2024-03-17 03:10:20 +0000

    Update aws-java-sdk-dynamodb, ... to 1.12.681

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -69,7 +69,7 @@ object Dependencies {
     val collectorPayload = "0.0.0"
     val schemaSniffer = "0.0.0"
 
-    val awsSdk = "1.12.676"
+    val awsSdk = "1.12.681"
     val gcpSdk = "2.31.0"
     val awsSdk2 = "2.25.6"
     val kinesisClient2 = "2.4.3"
