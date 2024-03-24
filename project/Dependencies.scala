20cafda27e7f1132620666e3b4d583725d562a55
Scala Steward <scala-steward@users.noreply.github.com>
2024-03-24 03:19:35 +0000

    Update netty-all, netty-codec to 4.1.108.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
     val log4j = "2.23.1"
     val thrift = "0.19.0"
     val sprayJson = "1.3.6"
-    val netty = "4.1.107.Final"
+    val netty = "4.1.108.Final"
     val protobuf = "3.25.3"
 
     val refererParser = "1.1.0"
