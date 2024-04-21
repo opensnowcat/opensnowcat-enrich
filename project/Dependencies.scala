907087a8869d8acd6160ef268ff29c94f47b9c3c
Scala Steward <scala-steward@users.noreply.github.com>
2024-04-21 03:13:16 +0000

    Update google-cloud-storage to 2.37.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -70,7 +70,7 @@ object Dependencies {
     val schemaSniffer = "0.0.0"
 
     val awsSdk = "1.12.701"
-    val gcpSdk = "2.31.0"
+    val gcpSdk = "2.37.0"
     val awsSdk2 = "2.25.31"
     val kinesisClient2 = "2.4.3"
     val kafka = "7.6.1-ce"
