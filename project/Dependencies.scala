56c1c03fc7f4bef16928599b7f31359521cd3f77
Scala Steward <scala-steward@users.noreply.github.com>
2024-05-05 03:30:47 +0000

    Update mysql-connector-j to 8.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object Dependencies {
     val uaParser = "1.6.1"
     val snakeYaml = "2.2"
     val postgresDriver = "42.7.3"
-    val mysqlConnector = "8.3.0"
+    val mysqlConnector = "8.4.0"
     val hikariCP = "5.1.0"
     val jaywayJsonpath = "2.7.0"
     val jsonsmart = "2.5.1"
