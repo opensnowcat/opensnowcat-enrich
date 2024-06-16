a9f08b4eb549b7e8e22e83fc53b0febaf58ce0cc
Scala Steward <scala-steward@users.noreply.github.com>
2024-06-16 03:47:43 +0000

    Update aws-msk-iam-auth to 2.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -74,7 +74,7 @@ object Dependencies {
     val awsSdk2 = "2.25.60"
     val kinesisClient2 = "2.4.3"
     val kafka = "7.6.1-ce"
-    val mskAuth = "2.1.0"
+    val mskAuth = "2.1.1"
     val nsqClient = "1.3.0"
     val jackson = "2.15.3"
     val config = "1.3.4"
