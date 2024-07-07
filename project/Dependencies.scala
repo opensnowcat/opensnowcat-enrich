f038d1c1d46e5f5721f22528e9b6db133f8f0e75
Scala Steward <scala-steward@users.noreply.github.com>
2024-07-07 03:33:20 +0000

    Update sentry to 7.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -101,7 +101,7 @@ object Dependencies {
     val scalacheck = "1.18.0"
     val testcontainers = "0.40.10"
     val parserCombinators = "2.4.0"
-    val sentry = "1.7.30"
+    val sentry = "7.11.0"
 
     val betterMonadicFor = "0.3.1"
   }
