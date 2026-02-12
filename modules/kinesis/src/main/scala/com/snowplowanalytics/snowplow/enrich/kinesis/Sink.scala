3a8be50af6b8565aaeebae45e0960666ba245b65
Oleh Yermolaiev <oleg.yermolaiev@sysgears.com>
2026-02-12 20:14:32 +0200

    Upgrade kinesis module to cats effect 3

--- modules/kinesis/src/main/scala/com/snowplowanalytics/snowplow/enrich/kinesis/Sink.scala
+++ modules/kinesis/src/main/scala/com/snowplowanalytics/snowplow/enrich/kinesis/Sink.scala
@@ -20,8 +20,8 @@ import scala.collection.JavaConverters._
 import cats.implicits._
 import cats.{Monoid, Parallel}
 
-import cats.effect.{Blocker, Concurrent, ContextShift, Resource, Sync, Timer}
-import cats.effect.concurrent.Ref
+import cats.effect.{Resource, Sync}
+import cats.effect.kernel.{Async, Ref}
 
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
@@ -43,16 +43,14 @@ object Sink {
   private implicit def unsafeLogger[F[_]: Sync]: Logger[F] =
     Slf4jLogger.getLogger[F]
 
-  def init[F[_]: Concurrent: ContextShift: Parallel: Timer](
-    blocker: Blocker,
+  def init[F[_]: Async: Parallel](
     output: Output
   ): Resource[F, ByteSink[F]] =
     for {
-      sink <- initAttributed(blocker, output)
+      sink <- initAttributed(output)
     } yield (records: List[Array[Byte]]) => sink(records.map(AttributedData(_, UUID.randomUUID().toString, Map.empty)))
 
-  def initAttributed[F[_]: Concurrent: ContextShift: Parallel: Timer](
-    blocker: Blocker,
+  def initAttributed[F[_]: Async: Parallel](
     output: Output
   ): Resource[F, AttributedByteSink[F]] =
     output match {
@@ -61,7 +59,7 @@ object Sink {
           case Some(region) =>
             for {
               producer <- Resource.eval[F, AmazonKinesis](mkProducer(o, region))
-            } yield records => writeToKinesis(blocker, o, producer, toKinesisRecords(records))
+            } yield records => writeToKinesis(o, producer, toKinesisRecords(records))
           case None =>
             Resource.eval(Sync[F].raiseError(new RuntimeException(s"Region not found in the config and in the runtime")))
         }
@@ -97,8 +95,7 @@ object Sink {
                 }
     } yield exists
 
-  private def writeToKinesis[F[_]: ContextShift: Parallel: Sync: Timer](
-    blocker: Blocker,
+  private def writeToKinesis[F[_]: Async: Parallel](
     config: Output.Kinesis,
     kinesis: AmazonKinesis,
     records: List[PutRecordsRequestEntry]
@@ -110,7 +107,7 @@ object Sink {
       for {
         records <- ref.get
         failures <- group(records, config.recordLimit, config.byteLimit, getRecordSize)
-                      .parTraverse(g => tryWriteToKinesis(blocker, config, kinesis, g, policyForErrors))
+                      .parTraverse(g => tryWriteToKinesis(config, kinesis, g, policyForErrors))
         flattened = failures.flatten
         _ <- ref.set(flattened)
       } yield flattened
@@ -120,7 +117,7 @@ object Sink {
       failures <- runAndCaptureFailures(ref)
                     .retryingOnFailures(
                       policy = policyForThrottling,
-                      wasSuccessful = _.isEmpty,
+                      wasSuccessful = l => Sync[F].pure(l.isEmpty),
                       onFailure = { case (result, retryDetails) =>
                         val msg = failureMessageForThrottling(result, config.streamName)
                         Logger[F].warn(s"$msg (${retryDetails.retriesSoFar} retries from cats-retry)")
@@ -174,20 +171,18 @@ object Sink {
    *  If we are throttled by kinesis, the list contains throttled records and records that gave internal errors.
    *  If there is an exception, or if all records give internal errors, then we retry using the policy.
    */
-  private def tryWriteToKinesis[F[_]: ContextShift: Sync: Timer](
-    blocker: Blocker,
+  private def tryWriteToKinesis[F[_]: Async](
     config: Output.Kinesis,
     kinesis: AmazonKinesis,
     records: List[PutRecordsRequestEntry],
     retryPolicy: RetryPolicy[F]
   ): F[Vector[PutRecordsRequestEntry]] =
     Logger[F].debug(s"Writing ${records.size} records to ${config.streamName}") *>
-      blocker
-        .blockOn(Sync[F].delay(putRecords(kinesis, config.streamName, records)))
+      Async[F].blocking(putRecords(kinesis, config.streamName, records))
         .map(TryBatchResult.build(records, _))
         .retryingOnFailuresAndAllErrors(
           policy = retryPolicy,
-          wasSuccessful = r => !r.shouldRetrySameBatch,
+          wasSuccessful = r => Sync[F].pure(!r.shouldRetrySameBatch),
           onFailure = { case (result, retryDetails) =>
             val msg = failureMessageForInternalErrors(records, config.streamName, result)
             Logger[F].error(s"$msg (${retryDetails.retriesSoFar} retries from cats-retry)")
