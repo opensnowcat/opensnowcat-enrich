451aba23235829000c4410122d95132cd170cd8f
Oleh Yermolaiev <oleg.yermolaiev@sysgears.com>
2026-02-13 15:15:15 +0200

    Use EntityEncoder for request body to set Content-Length
    Fixes "Broken pipe" errors after http4s Ember migration by avoiding
    chunked Transfer-Encoding for POST requests.

--- modules/common/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/utils/HttpClient.scala
+++ modules/common/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/utils/HttpClient.scala
@@ -14,10 +14,9 @@ package com.snowplowanalytics.snowplow.enrich.common.utils
 
 import cats.effect.kernel.Sync
 import cats.implicits._
-import fs2.Stream
 import org.http4s.client.{Client => Http4sClient}
-import org.http4s.headers.Authorization
-import org.http4s.{BasicCredentials, EmptyBody, EntityBody, Header, Headers, Method, Request, Status, Uri}
+import org.http4s.headers.{Authorization, `Content-Type`}
+import org.http4s.{BasicCredentials, EmptyBody, EntityEncoder, Header, Headers, MediaType, Method, Request, Status, Uri}
 import org.typelevel.ci.CIString
 
 trait HttpClient[F[_]] {
@@ -34,7 +33,6 @@ object HttpClient {
 
   private[utils] def getHeaders(authUser: Option[String], authPassword: Option[String]): Headers = {
     val alwaysIncludedHeaders = List(
-      Header.Raw(CIString("content-type"), "application/json"),
       Header.Raw(CIString("accept"), "*/*")
     )
     if (authUser.isDefined || authPassword.isDefined)
@@ -58,18 +56,24 @@ object HttpClient {
           case Left(parseFailure) =>
             Sync[F].pure(new IllegalArgumentException(s"uri [$uri] is not valid: ${parseFailure.sanitized}").asLeft[String])
           case Right(validUri) =>
-            val request = Request[F](
+            val baseRequest = Request[F](
               uri = validUri,
               method = Method.fromString(method).getOrElse(Method.GET),
-              body = body.fold[EntityBody[F]](EmptyBody)(s => Stream.emits(s.getBytes)),
+              body = EmptyBody,
               headers = getHeaders(authUser, authPassword)
             )
+            val request = body.fold(baseRequest) { bodyString =>
+              val encoder = EntityEncoder
+                .stringEncoder[F]
+                .withContentType(`Content-Type`(MediaType.application.json))
+              baseRequest.withEntity(bodyString)(encoder)
+            }
             http4sClient
               .run(request)
               .use[Either[Throwable, String]] { response =>
-                val body = response.bodyText.compile.string
+                val responseBody = response.bodyText.compile.string
                 response.status.responseClass match {
-                  case Status.Successful => body.map(_.asRight[Throwable])
+                  case Status.Successful => responseBody.map(_.asRight[Throwable])
                   case _ => Sync[F].pure(new Exception(s"Request failed with status ${response.status.code}").asLeft[String])
                 }
               }
