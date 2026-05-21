58fc17904d418c2384583e0967df5f0e39b793f1
Oleh Yermolaiev <oleg.yermolaiev@sysgears.com>
2026-05-21 19:33:17 +0300

    Patch postgresql jdbc driver against CVE-2026-42198, to 42.7.11 version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
     val useragent = "1.21"
     val uaParser = "1.6.1"
     val snakeYaml = "2.2"
-    val postgresDriver = "42.7.3"
+    val postgresDriver = "42.7.11"
     val mysqlConnector = "8.3.0"
     val hikariCP = "5.1.0"
     val jaywayJsonpath = "2.9.0"
