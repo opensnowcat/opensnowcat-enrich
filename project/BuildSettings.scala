49a396f824db787d311c33e9def8f59dad49ddf7
Oleh Yermolaiev <oermolaev@users.noreply.github.com>
2026-07-20 15:20:43 +0300

    Updates/aws libs (#347)
    * Bump AWS SDK, Kinesis client, MSK auth, and fs2-blob-storage versions
    
    * Configure default assembly strategy to "rename" for all LICENSE files

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -183,6 +183,7 @@ object BuildSettings {
       case x if x.endsWith(".types") => MergeStrategy.first
       case x if x.contains("netty") => MergeStrategy.first
       case x if x.endsWith("NOTICE") => MergeStrategy.rename
+      case x if x.contains("LICENSE") => MergeStrategy.rename
       case x =>
         val oldStrategy = (assembly / assemblyMergeStrategy).value
         oldStrategy(x)
